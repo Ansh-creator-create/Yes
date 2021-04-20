765820d1d28d3a3a166a05ab5ed837b304e350bf
Ansh-creator-create <79391724+Ansh-creator-create@users.noreply.github.com>
2021-04-20 19:38:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var first=("Ansh")
+var last=("Pratihast")
+console.log(first)
+console.log(last)
+var merge=first.concat(last);
+console.log(merge);
+var number=("7");
+var numb=("5");
+console.log(number);
+console.log(numb);
+var add= number + numb;
+console.log(add);
+var subtract= number - numb;
+console.log(subtract);
+var multiply= number * numb;
+console.log(multiply);
+var divide= number / numb;
+console.log(divide);
